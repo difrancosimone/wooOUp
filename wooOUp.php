d9dac49ecac8823fa49bfb7acfd445b76a86a6cd
Simone <difrancosimone@gmail.com>
2017-06-03 17:40:10 +0200

    Added manage product stock

--- wooOUp.php
+++ wooOUp.php
@@ -20,7 +20,7 @@ class wooOUp {
     * Server Address and Port of Backend Api
     */
     static function install(){
-      $wooOup_options = ['ip' => '127.0.0.1', 'port' => '80'];
+      $wooOup_options = ['address' => '127.0.0.1', 'port' => '80'];
       add_option('wooOUp_option', $wooOup_options);
     }
     /*
@@ -48,7 +48,7 @@ class wooOUp {
     		<table class="form-table">
     			<tr valign="top">
     			<th scope="row">Api Server Address</th>
-    			<td><input type="text" name="ip_api" value="<?php echo esc_html($options_wooOUp['ip']); ?>" /></td>
+    			<td><input type="text" name="address_api" value="<?php echo esc_html($options_wooOUp['address']); ?>" /></td>
           <th scope="row">Api Server Port</th>
     			<td><input type="text" name="port_api" value="<?php echo esc_html($options_wooOUp['port']); ?>" /></td>
     			</tr>
@@ -58,9 +58,9 @@ class wooOUp {
     	</div>
     <?php
     	//save worpdress plugin options
-  		if ($_POST['ip_api'] && $_POST['port_api']) {
+  		if ($_POST['address_api'] && $_POST['port_api']) {
         $options_wooOUp = get_option('wooOUp_option');
-        $newoption = ['ip' => $_POST['ip_api'], 'port' => $_POST['port_api']];
+        $newoption = ['address' => $_POST['address_api'], 'port' => $_POST['port_api']];
         update_option( 'wooOUp_option', $options_wooOUp, $newoption );
       }
     }
@@ -69,7 +69,6 @@ class wooOUp {
     */
     static function wooOUp_variation_option( $term ) {
         /*global $wpdb, $product;
-
         $result = $wpdb->get_col( "SELECT slug FROM {$wpdb->prefix}terms WHERE name = '$term'" );
         $term_slug = ( !empty( $result ) ) ? $result[0] : $term;
         //build query to get vars
@@ -91,7 +90,22 @@ class wooOUp {
         }
         return $term;*/
         echo $term;
-        print_r($variationsarray);
+        //print_r($variationsarray);
+    }
+    /*
+    * Getting quantity from laravel api of product
+    */
+    private function getApiProductQuantity($sku) {
+      // Create a client with a base URI GET REQEUEST
+      $client = new GuzzleHttp\Client(['base_uri' => 'http://samples.openweathermap.org/data/2.5/']);
+      $response = $client->get('weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1');
+      //$response = $client->request('GET', 'weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1');
+      //echo "ResponseCode: ".$response->getStatusCode()."<br>";
+      $body = $response->getBody();
+      // Implicitly cast the body to a string and echo it
+      $jsonRes = (string) $body;
+      $test = json_decode($jsonRes);
+      echo "<br>Longitude: ".$test->coord->lon."<br>";
     }
     /*
     * Function to check availability via Api
@@ -111,64 +125,58 @@ class wooOUp {
             array_push($variationsarray, $single_variation->get_sku());
             //print_r($variationsarray);
             //echo '<option  value="'.$value.'">'.implode(" / ", $single_variation->get_variation_attributes()).'-'.get_woocommerce_currency_symbol().$single_variation->price.'</option>';
-            global $isVariable;
-            $isVariable = true;
             ?>
               <script>
                 console.log("PLUGIN OK SKU-> <?php echo $single_variation->get_sku()." - ".implode(" / ", $single_variation->get_variation_attributes()); ?>");
               </script>
             <?php
           }
+          /*
+          * Getting quantities from laravel api
+          */
+          echo json_encode($variationsarray)."<br>";
+          $resultVariations = array();
+          foreach ($variationsarray as $variaz) {
+            //array_push($resultVariations, $variaz "=> 12");
+            $resultVariations = array_merge($resultVariations, array($variaz => '12'));
+          }
+          echo json_encode($resultVariations)."<br>";
+          /*
+          * Product page function - hook the dropdown variations menu to show only avalable products
+          */
+          add_filter( 'woocommerce_variation_option_name', array( 'wooOUp','wooOUp_variation_option') );
         } else {
-          global $isVariable;
-          $isVariable = false;
           ?>
             <script>
               console.log("PLUGIN OK SKU-> <?php echo $product->get_sku(); ?>");
               //console.log("PLUGIN OK SKU-> <?php echo $variationsarray[1]; ?>");
             </script>
           <?php
+          /*
+          * Product page function - hook the single product stock quantity
+          * $stock_quantity is the amount returned by the api
+          */
+          $stock_quantity = 15; //test
+          global $product;
+          wc_update_product_stock( $product, $stock_quantity );
+
+          /*add_action( 'woocommerce_product_set_stock', array( 'wooOUp','wooOUp_product_single_quantity') );
+          function wooOUp_product_single_quantity( $obj ) {
+          		// Get stock quantity
+          		$stock = $obj->get_stock_quantity();
+          		// Foreach translation, sync stock amount
+          			foreach ( $translations as $lang => $translation ) {
+          				$p_other = new WC_Product_Simple( $translation );
+          				$amount = $p_other->set_stock( $stock );
+          			}
+          		}
+          		// reset this filter
+          		add_filter( 'woocommerce_product_set_stock', 'willy_sync_stock_amount_pll' );
+          	}
+          }*/
         }
-        if ($isVariable){
-          echo "Has Variations";
-        } else {
-          echo "Single";
-        }
-        /*
-        * Getting quantities from laravel api
-        */
-        var_dump($variationsarray);
-        echo $variationsarray[3];
-        exit;
-        // Create a client with a base URI GET REQEUEST
-        $client = new GuzzleHttp\Client(['base_uri' => 'http://samples.openweathermap.org/data/2.5/']);
-        $response = $client->get('weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1');
-        //$response = $client->request('GET', 'weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1');
-        //echo "ResponseCode: ".$response->getStatusCode()."<br>";
-        $body = $response->getBody();
-        // Implicitly cast the body to a string and echo it
-        $jsonRes = (string) $body;
-        $test = json_decode($jsonRes);
-        echo "<br>Longitude: ".$test->coord->lon."<br>";
-        //POST REQ
-        /*$clientpost = new GuzzleHttp\Client(['base_uri' => 'http://laravelapi.it']);
-        $responsepost = $clientpost->post('v1/product', [
-          'debug' => TRUE,
-          'body' => json_encode($variationsarray),
-          'headers' => [
-            'Content-Type' => 'application/x-www-form-urlencoded',
-          ]
-        ]);
-        $bodypost = $responsepost->getBody();
-        print_r(json_decode((string) $bodypost));
-        /*
-        * Product page function - hook the dropdown variations menu to show only avalable products
-        */
-        add_filter( 'woocommerce_variation_option_name', array( 'wooOUp','wooOUp_variation_option') );
       }
     }
-
-
 
 }
 /*
