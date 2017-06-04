f28db0e48ca9c0359a0a7c3afe2458cc2568a034
Simone <difrancosimone@gmail.com>
2017-06-04 17:10:00 +0200

    Removed unused parts of code

--- wooOUp.php
+++ wooOUp.php
@@ -65,34 +65,6 @@ class wooOUp {
       }
     }
     /*
-    * Function that alter the dropdown vaiation menù by product availability
-    */
-    static function wooOUp_variation_option( $term ) {
-        global $wpdb, $product, $variationsarray;
-        var_dump($variationsarray); //test
-        //var_dump($term);
-        $result = $wpdb->get_col( "SELECT slug FROM {$wpdb->prefix}terms WHERE name = '$term'" );
-        $term_slug = ( !empty( $result ) ) ? $result[0] : $term;
-        //build query to get vars
-        $query = "SELECT postmeta.post_id AS product_id
-                    FROM {$wpdb->prefix}postmeta AS postmeta
-                        LEFT JOIN {$wpdb->prefix}posts AS products ON ( products.ID = postmeta.post_id )
-                    WHERE postmeta.meta_key LIKE 'attribute_%'
-                        AND postmeta.meta_value = '$term_slug'
-                        AND products.post_parent = $product->id";
-        $variation_id = $wpdb->get_col( $query );
-        $parent = wp_get_post_parent_id( $variation_id[0] );
-        if ( $parent > 0 ) {
-    	    $_product = new WC_Product_Variation( $variation_id[0] );
-      		if ($term) {
-      			// calculating itemized price
-      			$totsomma = $_product->get_price();
-      			return $term . ' --- ' .$totsomma;
-      		}
-        }
-        return $term;
-    }
-    /*
     * Getting quantity from laravel api of product
     */
     private function getApiProductQuantity($sku) {
@@ -146,10 +118,6 @@ class wooOUp {
               </script>
             <?php
           }
-          /*
-          * Product page function - hook the dropdown variations menu to show only avalable products
-          */
-          //add_filter( 'woocommerce_variation_option_name', array( 'wooOUp','wooOUp_variation_option') );
         } else {
           ?>
             <script>
